63e7e8caf148998601f3516645183ec24bf71b5c
HeartBeat <heartbeatpkmn@gmail.com>
2022-02-07 16:18:02 +0700

    video 10

--- app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/callbacks/MusicPlayerEventListener.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/callbacks/MusicPlayerEventListener.kt
@@ -22,9 +22,15 @@ class MusicPlayerEventListener(
 
     override fun onPlaybackStateChanged(playbackState: Int) {
         super.onPlaybackStateChanged(playbackState)
+        if(playbackState == Player.STATE_READY){
+            musicService.stopForeground(false)
+        }
     }
 
     override fun onPlayWhenReadyChanged(playWhenReady: Boolean, reason: Int) {
         super.onPlayWhenReadyChanged(playWhenReady, reason)
+        if(!playWhenReady){
+            musicService.stopForeground(false)
+        }
     }
 }
