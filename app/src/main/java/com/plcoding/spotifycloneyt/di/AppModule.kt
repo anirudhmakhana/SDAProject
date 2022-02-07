a9c57110e4ca8b57c51d1bdd17ba2a7f4a88d9fb
HeartBeat <heartbeatpkmn@gmail.com>
2022-02-07 11:12:46 +0700

    video 3

--- app/src/main/java/com/plcoding/spotifycloneyt/di/AppModule.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/di/AppModule.kt
@@ -6,13 +6,18 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.plcoding.spotifycloneyt.R
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule{
+
+    @Singleton
+    @Provides
     fun provideGlideInstance(
         @ApplicationContext context: Context
     ) = Glide.with(context).setDefaultRequestOptions(
