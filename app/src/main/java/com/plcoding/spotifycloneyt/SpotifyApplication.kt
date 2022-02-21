10196c67c634e1fc844422e1f08338fe8648056c
HeartBeat <heartbeatpkmn@gmail.com>
2022-02-21 18:17:47 +0700

    change back from MultiDexApp to normal App

--- app/src/main/java/com/plcoding/spotifycloneyt/SpotifyApplication.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/SpotifyApplication.kt
@@ -1,8 +1,7 @@
 package com.plcoding.spotifycloneyt
 
 import android.app.Application
-import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SpotifyApplication: MultiDexApplication()
+class SpotifyApplication: Application()
